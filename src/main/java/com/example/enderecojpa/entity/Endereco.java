2be106f7591602c877425ef910d74a95a2c069fe
HenriqueKaiser <henrique.kaiser@hotmail.com>
2023-06-21 22:18:23 -0300

    JPA

---
+++ src/main/java/com/example/enderecojpa/entity/Endereco.java
@@ -0,0 +1,27 @@
+package com.example.enderecojpa.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class Endereco {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String rua;
+    private String cidade;
+    private String estado;
+    private String cep;
+
+
+}
